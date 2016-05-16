9c73e3598c81dd435181d96d01ec12ced68a8fb3
steampilot <info@steampilot.ch>
2016-05-16 02:09:10 +0200

    created stored procedure for creating users

---
+++ sp_create_user.sql
@@ -0,0 +1,32 @@
+DELIMITER //
+CREATE PROCEDURE createUser(IN this_user VARCHAR(50), IN this_host VARCHAR(50), IN this_pass VARCHAR(50))
+  BEGIN
+    -- Make sure this user isn't allready created
+    IF (SELECT IFNULL((
+      SELECT 1
+      FROM mysql.user
+      WHERE User = this_user
+        AND Host = this_host), 0) = 0)
+    THEN
+      DECLARE query1 VARCHAR(255);
+      SET query1 = CONCAT('
+        CREATE USER "', this_user, '"@"', this_host, '" IDENTIFIED BY "', this_pass, '" '
+      );
+      PREPARE statement1 FROM query1;
+        EXECUTE statement1;
+        DEALLOCATE PREPARE statement1;
+      DECLARE query2 VARCHAR(255);
+      SET query2 = CONCAT('GRANT SELECT ON `schoolinfo_neu`
+      TO "', this_user, '"@"', this_host, '" IDENTIFIED BY "', this_pass, '"
+        WITH MAX_QUERIES_PER_HOUR 120
+        MAX_CONNECTIONS_PER_HOUR 60
+        MAX_UPDATES_PER_HOUR 60
+        MAX_USER_CONNECTIONS 2'
+      );
+      PREPARE statement2 FROM query2;
+        EXECUTE statement2;
+        DEALLOCATE PREPARE statement2;
+      SELECT CONCAT('User', this_user, ' at ', this_host, ' has been granted SELECT permissons');
+    END IF;
+  END //
+DELIMITER ;
